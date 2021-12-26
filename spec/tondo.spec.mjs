f82864d7b10533bced6c9d7911d7f643edd863ed
Cesare Jacopo Corzani <corzani@gmail.com>
2021-12-26 12:04:18 +0000

    WIP Tests

--- spec/tondo.spec.mjs
+++ spec/tondo.spec.mjs
@@ -1,37 +1,35 @@
 import Tondo from '../bin/tondo.js';
-import {randomPicture, randomPictures} from './usplashMocks.mjs'
+import { randomPicture, randomPictures } from './unsplashMocks.mjs'
 import { resolve } from 'path';
 
+const tondoMock = (data) => {
+  const downloadService = jasmine.createSpy().and.returnValue(Promise.resolve('./file.jpg'));
+  const setWallpaper = jasmine.createSpy();
+  const wallpaperService = () => Promise.resolve({ setWallpaper });
+  const log = jasmine.createSpy();
+  const axios = { get: jasmine.createSpy().and.returnValue(Promise.resolve({ data })) }
+  
+  return ({tondo: Tondo(axios, 'CLIENT_ID_TEST', wallpaperService, downloadService, log), axios, log, setWallpaper, downloadService })
+}
 
-describe("Player", function () {
+describe("Tondo", function () {
   it("should call setWallpaper", async function () {
 
-    const downloadService = jasmine.createSpy().and.returnValue(Promise.resolve('./file.jpg'));
-    const setWallpaper = { setWallpaper: jasmine.createSpy() };
-    const wallpaperService = () => Promise.resolve(setWallpaper);
-    const log = jasmine.createSpy();
-    const axios = { get: jasmine.createSpy().and.returnValue(Promise.resolve({ data: randomPicture })) }
-
-    const tondoInstance = Tondo(axios, 'CLIENT_ID_TEST', wallpaperService, downloadService, log)
-    const t = await tondoInstance.setRandomBackground("./", 'cat', 'portrait')
+    const {tondo, axios,downloadService, setWallpaper, log} = tondoMock(randomPicture);
+    await tondo.setRandomBackground("./", 'cat', 'portrait')
 
     expect(axios.get).toHaveBeenCalledWith('https://api.unsplash.com/photos/random',
       { params: { client_id: 'CLIENT_ID_TEST', orientation: 'portrait', query: 'cat', } });
 
     expect(downloadService).toHaveBeenCalledWith(axios, randomPicture, resolve('./', 'Dwu85P9SOIk.jpg'), 'CLIENT_ID_TEST');
-    expect(setWallpaper.setWallpaper).toHaveBeenCalledWith('./file.jpg');
+    expect(setWallpaper).toHaveBeenCalledWith('./file.jpg');
     expect(log).toHaveBeenCalled();
   });
 
   it("should call downloadAllToFolder", async function () {
-    const downloadService = jasmine.createSpy().and.returnValue(Promise.resolve('./file.jpg'));
-    const setWallpaper = { setWallpaper: jasmine.createSpy() };
-    const wallpaperService = () => Promise.resolve(setWallpaper);
-    const log = jasmine.createSpy();
-    const axios = { get: jasmine.createSpy().and.returnValue(Promise.resolve({ data: randomPictures })) }
+    const {tondo, axios,downloadService, log} = tondoMock(randomPictures);
 
-    const tondoInstance = Tondo(axios, 'CLIENT_ID_TEST', wallpaperService, downloadService, log)
-    const t = await tondoInstance.downloadAllToFolder({
+    await tondo.downloadAllToFolder({
       orientation: 'portrait',
       query: 'cat',
       count: 2
@@ -42,7 +40,7 @@ describe("Player", function () {
 
     expect(downloadService).toHaveBeenCalledWith(axios, randomPictures[0], resolve('./', 'Dwu85P9SOIk.jpg'), 'CLIENT_ID_TEST');
     expect(downloadService).toHaveBeenCalledWith(axios, randomPictures[1], resolve('./', 'Mwu55P9SOIk.jpg'), 'CLIENT_ID_TEST');
-
+    expect(downloadService).toHaveBeenCalledTimes(2);
     expect(log).toHaveBeenCalledTimes(2);
   });
 });
